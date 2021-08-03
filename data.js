19ff910c93448793d8e86d2fbc44eb2b0121cd06
jiyun jeon <jiyunjeon@jiyunui-MacBookPro.local>
2021-08-03 19:33:32 +0900

    g

--- data.js
+++ data.js
@@ -1,7 +1,54 @@
 var questions = [
     {
-        "question": "안녕!",
-        "true": "E",
-        "false": "I"
+        "question": "안녕1",
+        "answers" :{
+            true: {
+                "text": "당신은 I입니까?",
+                "value": "I"
+            },
+            false: {
+                "text": "당신은 E입니까?",
+                "value": "E"
+            }
+        }
+    },
+    {
+        "question": "안녕2",
+        "answers" :{
+            true: {
+                "text": "당신은 N입니까?",
+                "value": "N"
+            },
+            false: {
+                "text": "당신은 S입니까?",
+                "value": "S"
+            }
+        }
+    },
+    {
+        "question": "안녕3",
+        "answers" :{
+            true: {
+                "text": "당신은 T입니까?",
+                "value": "T"
+            },
+            false: {
+                "text": "당신은 F입니까?",
+                "value": "F"
+            }
+        }
+    },
+    {
+        "question": "안녕4",
+        "answers" :{
+            true: {
+                "text": "당신은 P입니까?",
+                "value": "P"
+            },
+            false: {
+                "text": "당신은 J입니까?",
+                "value": "J"
+            }
+        }
     }
 ]
